3107ce5074aec61e99d1d4d54defae3f10697037
nguyenconghieudata <hieunc@farmcode.io.vn>
2025-04-22 23:17:15 +0700

    update: APIs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,11 +5,11 @@ export const API = {
   //SLIDER
   GET_ALL_SLIDER: `${BASE_URL}/ielts-viet/slider/`,
   //BLOG
-  GET_ALL_BLOG: `${BASE_URL}/ielts-viet/blog/`,
+  GET_ALL_BLOG: `${BASE_URL}/ielts-viet/blog`,
   GET_BLOG_BY_ID: `${BASE_URL}/ielts-viet/blog`,
   GET_AUTHOR_BY_ID: `${BASE_URL}/ielts-viet/author`,
   //REVIEWS
-  GET_ALL_REVIEW: `${BASE_URL}/ielts-viet/review/`,
+  GET_ALL_REVIEW: `${BASE_URL}/ielts-viet/review`,
   //TIMEKEEPING
   GET_ALL_TEACHER: `${BASE_URL}/ielts-viet/account/`,
   CHECK_IN_OUT: `${BASE_URL}/ielts-viet/account/check`,
