2af649fe4e24307c275c0144b6b033ec9404a7c6
nguyenconghieudata <hieunc@farmcode.io.vn>
2025-08-30 21:00:12 +0700

    update: detail course

--- src/modules/course/components/detail-course.tsx
+++ src/modules/course/components/detail-course.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { CircleCheckBig, Trophy } from "lucide-react";
 import Image from "next/image";
@@ -17,45 +17,67 @@ export default function CourseDetail({ course }: any) {
             />
           </div>
           <div className="space-y-4 lg:space-y-6 lg:col-span-7">
-            <h1 className="text-2xl sm:text-3xl font-bold text-gray-900">{course?.course_name}</h1>
+            <h1 className="text-2xl sm:text-3xl font-bold text-gray-900">
+              {course?.course_name}
+            </h1>
             <div className="space-y-2 sm:space-y-3">
-              <div className="flex flex-wrap items-center gap-2 sm:gap-4">
-                <div className="flex items-center space-x-2">
-                  <Trophy className="w-4 h-4 sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))]" />
-                  <span className="text-sm sm:text-base font-semibold">Đầu vào: {course?.input_score}</span>
+              <div className="flex flex-col items-start gap-2 sm:gap-3">
+                <div className="grid grid-cols-12 items-center gap-6">
+                  <Trophy className="w-4 h-4 sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))] col-span-1" />
+                  <span className="text-sm sm:text-base font-semibold col-span-11">
+                    Đầu vào: {course?.input_score}
+                  </span>
                 </div>
-                <div className="flex items-center space-x-2">
-                  <Trophy className="w-4 h-4 sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))]" />
-                  <span className="text-sm sm:text-base font-semibold">Đầu ra: {course?.output_score}</span>
+                <div className="grid grid-cols-12 items-center gap-6">
+                  <Trophy className="w-4 h-4 sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))] col-span-1" />
+                  <span className="text-sm sm:text-base font-semibold col-span-11">
+                    Đầu ra: {course?.output_score}
+                  </span>
                 </div>
               </div>
               {course?.commission.map((item: any, index: number) => (
-                <div key={index} className="flex items-center space-x-2">
-                  <Trophy className="w-4 h-4 sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))]" />
-                  <span className="text-sm sm:text-base font-semibold">{item}</span>
+                <div
+                  key={index}
+                  className="grid grid-cols-12 items-center gap-0"
+                >
+                  <Trophy className="w-4 h-4 sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))] col-span-1" />
+                  <span className="text-sm sm:text-base font-semibold col-span-11">
+                    {item}
+                  </span>
                 </div>
               ))}
-
             </div>
             <div className="space-y-2 sm:space-y-3">
-              <div className="flex items-center space-x-2">
-                <CircleCheckBig size={16} className="sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))]" />
-                <span className="text-sm sm:text-base">Thời gian: {course?.study_time}</span>
+              <div className="grid grid-cols-12 items-center gap-0">
+                <CircleCheckBig
+                  size={16}
+                  className="sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))] col-span-1"
+                />
+                <span className="text-sm sm:text-base font-semibold col-span-11">
+                  Thời gian: {course?.study_time}
+                </span>
               </div>
-              <div className="flex items-center space-x-2">
-                <CircleCheckBig size={16} className="sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))]" />
-                <span className="text-sm sm:text-base">Thời lượng: {course?.duration}</span>
+              <div className="grid grid-cols-12 items-center gap-0">
+                <CircleCheckBig
+                  size={16}
+                  className="sm:w-5 sm:h-5 text-[rgb(var(--secondary-rgb))] col-span-1"
+                />
+                <span className="text-sm sm:text-base col-span-11">
+                  Thời lượng: {course?.duration}
+                </span>
               </div>
             </div>
           </div>
         </div>
         <div className="space-y-2 mt-10">
-          <h2 className="text-lg sm:text-xl font-semibold">Mục tiêu khóa học</h2>
+          <h2 className="text-lg sm:text-xl font-semibold">
+            Mục tiêu khóa học
+          </h2>
           <p className="text-sm sm:text-base text-gray-600 leading-relaxed">
             {course?.description}
           </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
